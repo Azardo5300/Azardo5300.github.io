748c65881d8fd29ba8678fc77f2c00cfeb9fffe0
Azardo5300 <106530452+Azardo5300@users.noreply.github.com>
2022-10-16 19:50:48 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+console.log("Hello world");
+menu.onclick = () =>{
+    navbar.classList.toggle('active')
+}
+
